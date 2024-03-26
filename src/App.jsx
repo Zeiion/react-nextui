50100e76ad33652154d8bf3e4963cec7a12f3638
Zeiion <zeiion@qq.com>
2024-03-26 16:34:32 +0800

    nextui

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,55 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Select, SelectItem } from "@nextui-org/react";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [v, setV] = useState([])
+
+  const numberOptions = [
+    {
+      label: "A",
+      value: 1,
+    },
+    {
+      label: "B",
+      value: 2,
+    },
+  ]
+
+  const stringOptions = numberOptions.map((o)=>({
+    ...o,
+    value: String(o.value),
+  }))
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      Hello World
+      <Select
+        selectedKeys={[1]}
+        items={numberOptions}
+      >
+        {
+          ({label, value})=> <SelectItem key={value}>{label}</SelectItem>
+        }
+      </Select>
+      <Select
+        selectedKeys={["1"]}
+        items={numberOptions}
+      >
+        {
+          ({label, value})=> <SelectItem key={value}>{label}</SelectItem>
+        }
+      </Select>
+      <Select
+        selectedKeys={["1"]}
+        items={stringOptions}
+      >
+        {
+          ({label, value})=> <SelectItem key={value}>{label}</SelectItem>
+        }
+      </Select>
     </>
   )
 }
